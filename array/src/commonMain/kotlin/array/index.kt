f2067d25a3e3c0da248478c9bf43bbb99485ed67
Elias Mårtenson <lokedhs@gmail.com>
2020-09-24 19:46:16 +0800

    Remove commented code

--- array/src/commonMain/kotlin/array/index.kt
+++ array/src/commonMain/kotlin/array/index.kt
@@ -26,14 +26,9 @@ private class IndexedArrayValue(val content: APLValue, indexValue: Array<Either<
                     offset += contentMult[i] * selection.value
                 }
                 is Either.Right -> {
-//                    selection.value.forEach { v ->
-//                        a.add(v.size)
-//                        destAxis.add(AxisValueAndOffset(i, contentMult[i]))
-//                    }
                     selection.value.dimensions.dimensions.forEach { v ->
                         a.add(v)
                     }
-//                    destAxis.add(AxisValueAndOffset(i, (i until (i + selection.value.dimensions.size)).map { contentMult[it] }.toIntArray()))
                     destAxis.add(
                         AxisValueAndOffset(
                             outputAxis,
@@ -52,15 +47,6 @@ private class IndexedArrayValue(val content: APLValue, indexValue: Array<Either<
     override fun valueAt(p: Int): APLValue {
         val positionArray = dimensions.positionFromIndex(p)
         var result = constantOffset
-//        positionArray.forEachIndexed { i, positionInAxis ->
-//            val axisValue = destToSourceAxis[i]
-//            val selection = indexValue[axisValue.sourceIndex]
-//            result += axisValue.multiplier * when (selection) {
-//                is Either.Left -> throw IllegalStateException("Should not need to compute single-dimension values")
-//                is Either.Right -> selection.value.values[positionInAxis]
-//            }
-//        }
-//        return content.valueAt(result)
         destToSourceAxis.forEach { dts ->
             val srcCoords = IntArray(dts.source.rank) { i -> positionArray[dts.sourceIndex + i] }
             val srcAxisPos = dts.source.intValueAt(dts.source.dimensions.indexFromPosition(srcCoords, dts.sourceMultipliers))
@@ -113,17 +99,6 @@ class ArrayIndex(val content: Instruction, val indexInstr: Instruction, pos: Pos
                     }
                 })
             }
-//            when (d.size) {
-//                0 -> {
-//                    Either.Left(v.ensureNumber(pos).asInt()
-//                        .also { posAlongAxis -> checkAxisPositionIsInRange(posAlongAxis, aDimensions, i, pos) })
-//                }
-//                1 -> Either.Right(IntArray(d[0]) { i2 ->
-//                    v.valueAt(i2).ensureNumber(pos).asInt()
-//                        .also { posAlongAxis -> checkAxisPositionIsInRange(posAlongAxis, aDimensions, i, pos) }
-//                })
-//                else -> throw InvalidDimensionsException("Invalid dimension in array index argument ${i}", pos)
-//            }
         }
 
         return IndexedArrayValue(contentValue, axis)
