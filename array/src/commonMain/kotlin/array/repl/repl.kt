b7465e31a1a3275c8aac0c3769623ab3246c0287
Elias Mårtenson <lokedhs@gmail.com>
2021-02-15 15:02:24 +0800

    New repl features
    New options allows the user to specify a file to load, choose whether the repl is run or not

--- array/src/commonMain/kotlin/array/repl/repl.kt
+++ array/src/commonMain/kotlin/array/repl/repl.kt
@@ -6,7 +6,10 @@ import array.options.InvalidOption
 import array.options.Option
 
 fun runRepl(args: Array<String>) {
-    val argParser = ArgParser(Option("lib-path", true, "Location of the KAP standard library"))
+    val argParser = ArgParser(
+        Option("lib-path", true, "Location of the KAP standard library"),
+        Option("load", true, "File to load"),
+        Option("no-repl", false, "Don't start the REPL"))
     val argResult = try {
         argParser.parse(args)
     } catch (e: InvalidOption) {
@@ -21,27 +24,36 @@ fun runRepl(args: Array<String>) {
                 print(s)
             }
         }
-        argResult["lib-path"]?.let { libPath ->
-            val libPathType = fileType(libPath)
-            if (libPathType != null && libPathType == FileNameType.DIRECTORY) {
-                addLibrarySearchPath(libPath)
-            } else {
-                println("Warning: ${libPath} is not a directory, ignoring")
-            }
+    }
+    argResult["lib-path"]?.let { libPath ->
+        val libPathType = fileType(libPath)
+        if (libPathType != null && libPathType === FileNameType.DIRECTORY) {
+            engine.addLibrarySearchPath(libPath)
+        } else {
+            println("Warning: ${libPath} is not a directory, ignoring")
+        }
+    }
+    argResult["load"]?.let { file ->
+        val loadFileType = fileType(file)
+        when (loadFileType) {
+            FileNameType.FILE -> engine.parseAndEval(FileSourceLocation(file), false).collapse()
+            FileNameType.DIRECTORY -> println("Warning: ${file} is a directory")
+            else -> println("Warning: ${file} is not a valid file")
         }
     }
 
-
-    val prompt = "> "
-    while (true) {
-        val line = keyboardInput.readString(prompt) ?: break
-        val stringTrimmed = line.trim()
-        if (stringTrimmed != "") {
-            try {
-                val result = engine.parseAndEval(StringSourceLocation(line), false)
-                println(result.formatted(FormatStyle.PRETTY))
-            } catch (e: APLGenericException) {
-                println(e.formattedError())
+    if (!argResult.containsKey("no-repl")) {
+        val prompt = "> "
+        while (true) {
+            val line = keyboardInput.readString(prompt) ?: break
+            val stringTrimmed = line.trim()
+            if (stringTrimmed != "") {
+                try {
+                    val result = engine.parseAndEval(StringSourceLocation(line), false)
+                    println(result.formatted(FormatStyle.PRETTY))
+                } catch (e: APLGenericException) {
+                    println(e.formattedError())
+                }
             }
         }
     }
