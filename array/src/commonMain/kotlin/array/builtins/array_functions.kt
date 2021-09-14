d9ac3ad7a95a2f6e7d6c2f5d08a83548aa161009
Elias Martenson <elias@dhsdevelopments.com>
2021-09-14 18:02:40 +0800

    Improvement of index lookup

--- array/src/commonMain/kotlin/array/builtins/array_functions.kt
+++ array/src/commonMain/kotlin/array/builtins/array_functions.kt
@@ -626,27 +626,85 @@ class ConcatenateAPLFunctionLastAxis : APLFunctionDescriptor {
 }
 
 class AccessFromIndexAPLFunction : APLFunctionDescriptor {
-    class AccessFromIndexAPLFunctionImpl(pos: Position) : NoAxisAPLFunction(pos) {
-        override fun eval2Arg(context: RuntimeContext, a: APLValue, b: APLValue): APLValue {
+    class AccessFromIndexAPLFunctionImpl(pos: Position) : APLFunction(pos) {
+        override fun eval2Arg(context: RuntimeContext, a: APLValue, b: APLValue, axis: APLValue?): APLValue {
             val aFixed = a.arrayify()
             val ad = aFixed.dimensions
+
             if (ad.size != 1) {
                 throwAPLException(InvalidDimensionsException("Position argument is not rank 1", pos))
             }
+
             val bd = b.dimensions
-            if (ad[0] != bd.size) {
-                throwAPLException(
-                    InvalidDimensionsException(
-                        "Number of values in position argument must match the number of dimensions",
-                        pos))
+            val indexList = if (axis == null) {
+                if (ad[0] > bd.size) {
+                    throwAPLException(
+                        InvalidDimensionsException(
+                            "Number of values in position argument must be equal or less than the number of dimensions", pos))
+                }
+                Array(bd.size) { i ->
+                    if (i < ad[0]) {
+                        val indexValue = aFixed.valueAt(i)
+                        if (indexValue.dimensions.size == 0) {
+                            Either.Left(
+                                indexValue.ensureNumber(pos).asInt()
+                                    .also { posAlongAxis -> checkAxisPositionIsInRange(posAlongAxis, bd, i, pos) })
+                        } else {
+                            Either.Right(IntArrayValue.fromAPLValue(indexValue, pos))
+                        }
+                    } else {
+                        makeAllIndexList(bd[i])
+                    }
+                }
+            } else {
+                val list = ArrayList<Either<Int, IntArrayValue>?>()
+                repeat(bd.size) { list.add(null) }
+                val axesArray = axis.arrayify().toIntArray(pos)
+                if (axesArray.any { it < 0 || it >= bd.size }) {
+                    throwAPLException(IllegalAxisException("Invalid axis in axis specification", pos))
+                }
+                if (containsDuplicates(axesArray)) {
+                    throwAPLException(IllegalAxisException("Duplicated axis in axis specification", pos))
+                }
+                if (ad[0] != axesArray.size) {
+                    throwAPLException(
+                        IllegalAxisException(
+                            "Number of values in position argument must match the number of axes in axis specification", pos))
+                }
+                aFixed.iterateMembersWithPosition { m, p ->
+                    val axisInt = axesArray[p]
+                    val v = if (m.dimensions.size == 0) {
+                        Either.Left(m.ensureNumber(pos).asInt()
+                                        .also { posAlongAxis -> checkAxisPositionIsInRange(posAlongAxis, bd, axisInt, pos) })
+                    } else {
+                        Either.Right(IntArrayValue.fromAPLValue(m, pos))
+                    }
+                    list[axisInt] = v
+                }
+                Array(bd.size) { i ->
+                    list[i] ?: makeAllIndexList(bd[i])
+                }
             }
-            val posList = IntArray(ad[0]) { i -> aFixed.valueAtInt(i, pos) }
-            val p = bd.indexFromPosition(posList)
-            return b.valueAt(p)
+            return IndexedArrayValue(b, indexList)
+        }
+
+        private fun makeAllIndexList(size: Int): Either.Right<IntArrayValue> {
+            val allIndexList = IntArrayValue.fromAPLValue(IotaArrayImpls.IotaArrayLong(size))
+            return Either.Right(allIndexList)
+        }
+
+        private fun containsDuplicates(values: IntArray): Boolean {
+            for (i0 in 1 until values.size) {
+                val v0 = values[i0]
+                for (i1 in 0 until i0) {
+                    if (values[i1] == v0) return true
+                }
+            }
+            return false
         }
     }
 
-    override fun make(pos: Position) = AccessFromIndexAPLFunctionImpl(pos)
+    override fun make(pos: Position) = AccessFromIndexAPLFunctionImpl(pos.withName("Lookup index"))
 }
 
 class TakeArrayValue(val selection: IntArray, val source: APLValue, val pos: Position? = null) : APLArray() {
@@ -1088,7 +1146,7 @@ class CompareFunction : APLFunctionDescriptor {
                         var currentSize = 0
                         v.iterateMembers { inner ->
                             val size = recurse(inner)
-                            if(first) {
+                            if (first) {
                                 currentSize = size
                                 first = false
                             } else {
--- array/src/commonTest/kotlin/array/ArrayLookupTest.kt
+++ array/src/commonTest/kotlin/array/ArrayLookupTest.kt
@@ -29,4 +29,103 @@ class ArrayLookupTest : APLTest() {
             parseAPLExpression("2 3 ⌷ 1 2 3 4").collapse()
         }
     }
+
+    @Test
+    fun multiDimensionLookup() {
+        parseAPLExpression("3 4 ⌷ 4 5 ⍴ 100+⍳100").let { result ->
+            assertSimpleNumber(119, result)
+        }
+    }
+
+    @Test
+    fun multiValueLookup0() {
+        parseAPLExpression("(⊂ 5 10) ⌷ 100+⍳100").let { result ->
+            assertDimension(dimensionsOfSize(2), result)
+            assertArrayContent(arrayOf(105, 110), result)
+        }
+    }
+
+    @Test
+    fun multiValueLookup1() {
+        parseAPLExpression("(3 0) (3 2) ⌷ 4 5⍴100+⍳100").let { result ->
+            assertDimension(dimensionsOfSize(2, 2), result)
+            assertArrayContent(arrayOf(118, 117, 103, 102), result)
+        }
+    }
+
+    @Test
+    fun lookupMajorAxis() {
+        parseAPLExpression("0 ⌷ 4 4⍴100+⍳100").let { result ->
+            assertDimension(dimensionsOfSize(4), result)
+            assertArrayContent(arrayOf(100, 101, 102, 103), result)
+        }
+    }
+
+    @Test
+    fun lookupMajorAxisExplicit() {
+        parseAPLExpression("0 ⌷[0] 4 4⍴100+⍳100").let { result ->
+            assertDimension(dimensionsOfSize(4), result)
+            assertArrayContent(arrayOf(100, 101, 102, 103), result)
+        }
+    }
+
+    @Test
+    fun lookupOtherAxisExplicit() {
+        parseAPLExpression("0 ⌷[1] 4 4⍴100+⍳100").let { result ->
+            assertDimension(dimensionsOfSize(4), result)
+            assertArrayContent(arrayOf(100, 104, 108, 112), result)
+        }
+    }
+
+    @Test
+    fun multiValueLookupMajorAxis() {
+        parseAPLExpression("(⊂ 3 0) ⌷ 4 6 ⍴ 100+⍳100").let { result ->
+            assertDimension(dimensionsOfSize(2, 6), result)
+            assertArrayContent(arrayOf(118, 119, 120, 121, 122, 123, 100, 101, 102, 103, 104, 105), result)
+        }
+    }
+
+    @Test
+    fun lookupMultiAxes() {
+        parseAPLExpression("0 1 ⌷[0 1] 4 4 4 ⍴ 100+⍳100").let { result ->
+            assertDimension(dimensionsOfSize(4), result)
+            assertArrayContent(arrayOf(104,105,106,107), result)
+        }
+    }
+
+    @Test
+    fun lookupMultiAxesIllegalAxis() {
+        assertFailsWith<IllegalAxisException> {
+            parseAPLExpression("(0 1) (3 1) ⌷[0 3] 4 4 4 ⍴ 100+⍳100")
+        }
+    }
+
+    @Test
+    fun lookupMultiAxesNotEnoughArguments() {
+        assertFailsWith<IllegalAxisException> {
+            parseAPLExpression("(0 1) (3 1) ⌷[0] 4 4 4 ⍴ 100+⍳100")
+        }
+    }
+
+    @Test
+    fun lookupMultiAxesTooManyAxes() {
+        assertFailsWith<IllegalAxisException> {
+            parseAPLExpression("(0 1) (2 3) ⌷[0 1 2 3] 10 4 4 4 ⍴ 100+⍳100")
+        }
+    }
+
+    @Test
+    fun multiValueLookupWithIndexOutOfRange() {
+        assertFailsWith<APLIndexOutOfBoundsException> {
+            parseAPLExpression("(⊂ 11 100) ⌷ 100+⍳100")
+        }
+    }
+
+
+    @Test
+    fun duplicatedAxes() {
+        assertFailsWith<IllegalAxisException> {
+            parseAPLExpression("1 2 ⌷[1 1] 4 4⍴100+⍳100")
+        }
+    }
 }
